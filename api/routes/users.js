581ae7529b71d948b2ab42f4b6a5c9ab759203e4
SasakiMz <maximechoisay3@gmail.com>
2026-03-04 00:28:52 +0100

    remaniement de lemplacement de certains fichiers de la racine du projet à API

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const service = require('../services/users');
+const service = require('.././services/users');
 router.get('/:id', service.getById);
 router.put('/:id', service.add);
 router.patch('/:id', service.update);
