4120f2d72ab9e8a17b97f5c6de3b56200e69e825
SasakiMz <maximechoisay3@gmail.com>
2026-03-03 23:25:13 +0100

    Modèle de donnés défini, installation du npm bcrypt, Création du répertoire services avec des callbacks afin de pouvoir supprimer, ajouter des utilisateurs

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const mongoose  = require('mongoose');
+const Schema    = mongoose.Schema;
+const bcrypt    = require('bcrypt');
+
+const User  = new Schema({
+    name: {
+        type    : String,
+        trim    : true,
+        require : [true, 'le nom est requis'],
+    },
+    firstname: {
+        type: String,
+        trim: true,
+    },
+    email: {
+        type    : String,
+        true    : true,
+        required: [true,'L`email est requis'],
+        unique  : true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        trim: true,
+    }
+},{
+    timestamps: true,
+});
+
+User.pre('save', function(next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    this.password = bcrypt.hashSync(this.password, 10);
+    next();
+});
+
+module.exports = mongoose.model('User', User);
---
+++ services/users.js
@@ -0,0 +1,84 @@
+const User = require ('../models/user');
+
+//Callback servant à ajouter un utilisateur avec son id
+exports.getById = async (req, res, next) => {
+    const id = req.params.id
+    
+    try {
+        let user = await User.findById(id);
+
+        if (user) {
+            return res.status(200).json(user);
+        }
+
+        return res.status(404).json('user_not_found');
+    } catch (error) {
+        return res.status(501).json(error);
+    }
+}
+
+
+//  Callback servant à ajouter un utilisateur
+
+exports.add = async (req, res, next) => {
+
+    const temp = ({
+        name        : req.body.name,
+        firstname   : render.body.firstname,
+        email       : render.body.email,
+        password    : render.body.password,
+    });
+
+    try {
+        let user = await User.create(temp);
+
+        return res.status(201).json(user);
+    }   catch (error) {
+        return res.status(501).json(error);
+    }
+}
+
+
+//  Callback servant à modifier un utilisateur
+
+exports.update = async (req, res, next) => {
+    const id = req.params.id
+    const temp = ({
+        name        :req.body.name,
+        firstname   :req.body.firstname,
+        email       :req.body.email,
+        password    :req.body.password,
+    });
+
+    try {
+        let user = await User.findOne ({_id : id});
+    
+        if (user) {
+            Object.keys(temp).forEach((key) => {
+                if (!!temp[key]) {
+                    user[key] = temp[key];
+                }
+            });
+            
+            await user.save();
+            return res.status(201).json(error);
+        }
+            return res.status(404).json('user_not_found');
+    }   catch (error) {
+        return res.status(501).json(error);
+    }
+}
+
+//  Callback servant à supprimer un utilisateur
+
+exports.delete = async (req, res, next) => {
+    const id = req.params.id
+
+    try {
+        await User.deleteOne({_id: id});
+
+        return res.status(204).json('delete_ok');
+    } catch (error) {
+        return res.status(501).json(error);
+    }
+}
