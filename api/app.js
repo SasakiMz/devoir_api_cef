934a710f1cb23b96dbb07adb499d2ea9d2d47a1a
SasakiMz <maximechoisay3@gmail.com>
2026-03-03 17:29:02 +0100

    Installation de cors et de MongoDb, ajout de la variable d\environnement URL_MONGO dans .env, initialisation de la connexion à mongodb, déclaration des routes

--- api/app.js
+++ api/app.js
@@ -1,20 +1,31 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors  = require('cors');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const mongodb = require('./db/mongo.js');
+
+mongodb.initClientDbConnection();
+
+const app = express();
+
+app.use(cors({
+    exposedHeaders: ['Authorization'],
+    origin: '*'
+
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use(function(req, res, next) {
+    res.status(404).json({name: 'API', version: '1.0', status : 404, message: 'not_found'});
+});
 
 module.exports = app;
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const service = require('../services/users');
+router.get('/:id', service.getById);
+router.put('/:id', service.add);
+router.patch('/:id', service.update);
+router.delete('/:id', service.delete);
 
 module.exports = router;
